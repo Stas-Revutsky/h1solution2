921c50ea05827a4a40178c0e573615fae9784f7f
Admin <stas.revutsky@gmail.com>
2023-05-23 20:59:52 +0300

    Add project files.

---
+++ h1solution2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace h1solution2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+            Console.WriteLine("What is your name ?");
+            name = Console.ReadLine() ?? string.Empty;
+            do
+            {
+                Console.WriteLine($"How old are you, {name}?");
+            } 
+            while (!int.TryParse(Console.ReadLine(), out age));
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+        }
+    }
+}
